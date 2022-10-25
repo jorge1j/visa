47f6e0fbe16a7b013401f519d8352f44944a39e3
Jorge Nwajei <george2flex@yahoo.com>
2022-10-25 07:53:04 +0000

    devops hopes

---
+++ class.sh
@@ -0,0 +1,9 @@
+#My request for 2023
+#God grant my request
+
+echo "devops is simple but reqire hard work"
+echo "2023 i am going to be a devops engr"
+echo i will be very rich"
+echo "this message is for 'george'"
+echo "and i approve this message
+date 
